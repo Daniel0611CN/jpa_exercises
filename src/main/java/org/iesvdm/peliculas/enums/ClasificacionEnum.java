800e00ef1923b294087abc13571f269cc2170b92
Dani Clavijo <daniclavijonunez@gmail.com>
2025-02-19 19:05:48 +0100

    First Commit

---
+++ src/main/java/org/iesvdm/peliculas/enums/ClasificacionEnum.java
@@ -0,0 +1,9 @@
+package org.iesvdm.peliculas.enums;
+
+public enum ClasificacionEnum {
+
+    INFANTIL,
+    JUVENIL,
+    ADULTO
+
+}
---
+++ src/test/java/org/iesvdm/peliculas/PeliculasJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iesvdm.peliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeliculasJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
